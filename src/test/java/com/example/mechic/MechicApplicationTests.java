a3ca8e4e5d84b3b8893066488a159bbdd42f044d
echen12 <chen.edmond13@gmail.com>
2022-08-22 23:41:15 -0700

    first commit

---
+++ src/test/java/com/example/mechic/MechicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mechic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MechicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
